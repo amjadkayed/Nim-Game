18b0b494389c7830370c6c1d7c40d9a8aebd77bf
Amjad <amjad.kayed@hotmail.com>
2023-11-03 04:17:50 +0200

    init title of the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import "./App.css";
+import { Grid } from "@mui/material";
+import NimGameTitle from "./assets/NimGameTitle";
 
 function App() {
-
-  return <h1>hello adam :)</h1>;
+  return (
+    <Grid
+      container
+      alignContent={"center"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      justifyItems={"center"}
+      width={"100%"}
+      height={"100%"}
+    >
+      <NimGameTitle height="50px" />
+    </Grid>
+  );
 }
-  
+
 export default App;
